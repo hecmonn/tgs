2e19b0cd93dc38d4e446a94c65c71b7179b00ea4
hecmonn <hecmonn@gmail.com>
2017-11-13 21:49:53 -0600

    fixed carousel bug

--- src/components/Home/Tours/Tour.js
+++ src/components/Home/Tours/Tour.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import {Image} from 'react-native';
 import {Content,Header,Text,Button,Left,Right,Body,Icon,Footer,FooterTab} from 'native-base';
-import Carousel from './Carousel';
+//import Carousel from './Carousel';
 
 class Tour extends React.Component {
     constructor(props){
@@ -24,7 +24,6 @@ class Tour extends React.Component {
                 </Header>
                 <Body>
                     <Text>Tour description</Text>
-                    <Carousel />
                 </Body>
                 <Footer>
                     <FooterTab>
